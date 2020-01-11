19038a564e364bdd54b74b89299fbf41e559f1b4
Stephen Cook <stephen.naturalaffinity@gmail.com>
2020-01-11 14:08:48 -0500

    google search bar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
 		super();
 		this.state = {
       weatherData: null,
-      location: ''
+      location: '',
+      searchQuery: ''
 		}
   }
   componentDidMount() {
@@ -29,6 +30,16 @@ class App extends React.Component {
   }
   changeLocation = (loc) => {
     this.setState({ location: loc });
+  }
+  changeQuery = (e) => {
+    this.setState({ searchQuery: e.target.value });
+  }
+  searchGoogle = (e, searchArrowClicked) => {
+    if (this.state.searchQuery !== '') {
+      if (searchArrowClicked || e.key === 'Enter') {
+        window.open(`https://google.com/search?q=${this.state.searchQuery}`);
+      }
+    }
   }
   reverseGeocode = (lat, long) => {
     axios.get(`https://www.mapquestapi.com/geocoding/v1/reverse?key=iGU4SqMrHyMr2tIFRCu36SkN3n2uUNtj&location=${lat}%2C${long}&outFormat=json&thumbMaps=false`)
@@ -52,7 +63,27 @@ class App extends React.Component {
     return (
       <div className="h-screen bg-center bg-cover" style={{ backgroundImage:`url("https://images.unsplash.com/photo-1501612272219-9f77e47daef5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80")` }}>
         <section className="flex flex-col justify-between h-full px-24 pt-16 pb-24">
-          <span className="text-2xl font-semibold tracking-wider text-white">weather.app</span>
+          <div>
+            <span className="text-2xl font-semibold tracking-wider text-white">weather.app</span>
+            <div className="relative w-1/6 mt-5">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-4">
+                <svg className="w-5 h-5" viewBox="0 0 533.5 544.3">
+                  <defs/>
+                  <path fill="#4285f4" d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"/>
+                  <path fill="#34a853" d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"/>
+                  <path fill="#fbbc04" d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"/>
+                  <path fill="#ea4335" d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"/>
+                </svg>
+              </div>
+              <input onKeyPress={ this.searchGoogle } onChange={ this.changeQuery } className="block w-full px-12 py-2 text-gray-900 bg-white rounded-full focus:shadow-outline" placeholder="Search with Google" />
+              <div className="absolute inset-y-0 right-0 flex items-center pr-4">
+                <svg onClick={ () => this.searchGoogle(null, true) } className="w-5 h-5 text-gray-600 cursor-pointer fill-current" viewBox="0 0 24 24">
+                  <defs/>
+                  <path d="M18.59 13H3a1 1 0 010-2h15.59l-5.3-5.3a1 1 0 111.42-1.4l7 7a1 1 0 010 1.4l-7 7a1 1 0 01-1.42-1.4l5.3-5.3z" className="heroicon-ui"/>
+                </svg>
+              </div>
+            </div>
+          </div>
           <div className="flex items-center font-semibold leading-none text-white">
             <span className="text-6xl">{ this.state.weatherData ? this.state.weatherData.currently.temperature.toFixed() + '°' : '...'}</span>
             <div className="px-16">
@@ -68,7 +99,7 @@ class App extends React.Component {
             </div>
           </div>
         </section>
-        <section className="absolute top-0 right-0 h-full shadow-xl" style={{ width: '780px' }}>
+        <section className="absolute top-0 right-0 h-full shadow-xl" style={{ width: '735px' }}>
           <Sidebar weatherData={ this.state.weatherData } location={ this.state.location } changeLocation={ this.changeLocation } fetchWeatherData={ this.fetchWeatherData } />
         </section>
       </div>
